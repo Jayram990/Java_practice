2cdbc63a143684e6bff6970845eb860ede931395
Jay <test@gmail.com>
2024-07-21 21:13:37 +0530

    july21-second-commit

--- src/myfiles/TestPractice.java
+++ src/myfiles/TestPractice.java
@@ -2,28 +2,14 @@ package myfiles;
 
 import java.util.*;
 
-public class TestPractice extends TestJava {
+public class TestPractice {
 
 	public static void main(String[] args) {
 		
-	int n = 5;
-	int count = 1;	
-	for(int i = 1;i<=n;i++)
 		
-	{
-		for(int j=-1;j<=3;j++)
-		{
-			count = count+3;
-			System.out.print(count+ " " );
-			
-		}
-		
-		
-		System.out.println();
-	}
-		
-	}
 
+	
+	}
 }
 		
 			
